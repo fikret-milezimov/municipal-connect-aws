fc182d4dcd31a710149ff1dca8a044a9c5ef1d96
fetata <fetata_89@abv.bg>
2026-04-02 06:15:52 +0300

    feat: added docker

--- municipalConnect/settings.py
+++ municipalConnect/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
+
 
 
 
